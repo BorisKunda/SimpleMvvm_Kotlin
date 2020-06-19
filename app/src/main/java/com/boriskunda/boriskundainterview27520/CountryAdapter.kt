7900769578703eb48054c6fca340b8b9fc86ad01
BorisKunda <bskunda1992@gmail.com>
2020-06-19 20:47:34 +0300

    added - databinding - part1 - list screen + click listeners

--- app/src/main/java/com/boriskunda/boriskundainterview27520/CountryAdapter.kt
+++ app/src/main/java/com/boriskunda/boriskundainterview27520/CountryAdapter.kt
@@ -1,14 +1,10 @@
 package com.boriskunda.boriskundainterview27520
 
-import android.app.Activity
-import android.net.Uri
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou
+import com.boriskunda.boriskundainterview27520.databinding.ItemCountryLlBinding
 import java.util.*
 
 class CountryAdapter(
@@ -20,8 +16,9 @@ class CountryAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
         val inflater = LayoutInflater.from(parent.context)
-        val view: View = inflater.inflate(R.layout.item_country_ll, parent, false)
-        return VH(view)
+        val bindingVH: ItemCountryLlBinding =
+            DataBindingUtil.inflate(inflater, R.layout.item_country_ll, parent, false)
+        return VH(bindingVH)
     }
 
     override fun getItemCount(): Int {
@@ -31,22 +28,13 @@ class CountryAdapter(
     override fun onBindViewHolder(holder: VH, position: Int) {
 
         val country = countriesList[position]
-        holder.itemTv.text = country.name
-
-        //glide implementation for SVG images
-        GlideToVectorYou.justLoadImage(holder.itemIv.context as Activity, Uri.parse(country.flagUrl), holder.itemIv)
-
-        holder.itemView.setOnClickListener {
-            onRVItemClickListener.onRVItemClick(countriesList[position])
-        }
+        holder.binding.country = country
+        holder.binding.rvItemClickListener = onRVItemClickListener
 
     }
 
-    class VH(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        val itemTv: TextView = itemView.findViewById(R.id.item_tv)
-        val itemIv: ImageView = itemView.findViewById(R.id.item_iv)
-
+    class VH(bindingItem: ItemCountryLlBinding) : RecyclerView.ViewHolder(bindingItem.root) {
+        var binding: ItemCountryLlBinding = bindingItem
     }
 
     interface OnRVItemClickListener {
